b565149c4c1012d2f8938422daf1568630c485f5
lyoman <leomuchenje@gmail.com>
2020-12-17 10:17:41 +0200

    algebra links

--- src/app/training/algebra/algebra.page.ts
+++ src/app/training/algebra/algebra.page.ts
@@ -9,29 +9,29 @@ export class AlgebraPage implements OnInit {
 
   videos = [
     {
-      title: 'Introduction to long division',
-      description: 'Introduction to long division',
-      url: 'https://www.youtube.com/embed/52ZlXsFJULI',
+      title: 'Introduction to algebra',
+      description: 'What are variables, expressions, and equations? | Introduction to algebra | Algebra I | Khan Academy',
+      url: 'https://www.youtube.com/embed/vDqOoI-4Z6M?list=PL7AF1C14AF1B05894',
     },
     {
-      title: 'Introduction to long division',
-      description: 'Introduction to long division',
-      url: 'https://www.youtube.com/embed/52ZlXsFJULI',
+      title: 'How to solve equations of the form ax = b',
+      description: 'How to solve equations of the form ax = b | Linear equations | Algebra I | Khan Academy',
+      url: 'https://www.youtube.com/embed/9Ek61w1LxSc?list=PL7AF1C14AF1B05894',
     },
     {
-      title: 'Introduction to long division',
-      description: 'Introduction to long division',
-      url: 'https://www.youtube.com/embed/52ZlXsFJULI',
+      title: 'Solving a more complicated equation',
+      description: 'Solving a more complicated equation | Linear equations | Algebra I | Khan Academy',
+      url: 'https://www.youtube.com/embed/XoEn1LfVoTo?list=PL7AF1C14AF1B05894',
     },
     {
-      title: 'Introduction to long division',
-      description: 'Introduction to long division',
-      url: 'https://www.youtube.com/embed/52ZlXsFJULI',
+      title: 'Introduction to solving an equation with variables on both sides',
+      description: 'Introduction to solving an equation with variables on both sides | Algebra I | Khan Academy',
+      url: 'https://www.youtube.com/embed/f15zA0PhSek?list=PL7AF1C14AF1B05894',
     },
     {
-      title: 'Introduction to long division',
-      description: 'Introduction to long division',
-      url: 'https://www.youtube.com/embed/52ZlXsFJULI',
+      title: 'Algebra: Linear equations 4',
+      description: 'Algebra: Linear equations 4 | Linear equations | Algebra I | Khan Academy',
+      url: 'https://www.youtube.com/embed/9IUEk9fn2Vs?list=PL7AF1C14AF1B05894',
     },
   ];
 
